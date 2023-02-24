f94113f50ba42f51ff6921098b1a851aa7ea3934
pratish <pratish.joshi@krafters.dev>
2023-02-25 01:28:41 +0545

    initial commit: backend done

---
+++ server/app/Exceptions/Handler.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use App\Http\Traits\JSONResponseTrait;
+
+class Handler extends ExceptionHandler
+{
+    use JSONResponseTrait;
+
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register()
+    {
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return $this->jsonResponse(0, null, 'Route not found', null, 404);
+            }
+        });
+    }
+
+    public function render($request, Exception|Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            return $this->jsonResponse(0, null, 'No data found', null, 200);
+        }
+        return parent::render($request, $exception);
+    }
+}
